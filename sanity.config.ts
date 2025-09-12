63ad7b3ce4e438bdd11bc4c450309ebfbb591337
Ahmed Elshazly <ahmed.shazly15@gmail.com>
2025-09-12 14:49:22 +0400

    feat: enable custom Sanity desk structure

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,10 +2,11 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import project from './src/sanity/schemaTypes/project'
 import {projectId, dataset} from './src/sanity/env'
+import {structure} from './src/sanity/structure'
 
 export default defineConfig({
   projectId,
   dataset,
-  plugins: [deskTool()],
+  plugins: [deskTool({structure})],
   schema: {types: [project]},
 })
