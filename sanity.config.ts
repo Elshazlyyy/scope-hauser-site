70015f93d4fd9e5b1c26ea07721209c6204e74b6
Ahmed Elshazly <ahmed.shazly15@gmail.com>
2025-09-13 13:08:10 +0400

    CMS

---
+++ sanity.config.ts
@@ -0,0 +1,14 @@
+import {defineConfig} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {structureTool} from 'sanity/structure'
+import {schemaTypes} from './sanity/schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Scope Hauser CMS',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  basePath: '/studio',
+  plugins: [structureTool(), visionTool()],
+  schema: { types: schemaTypes },
+})
